9305a3c2e5c9a3d7ba5e3228edbf93895669298b
Will Newton <91620451+WilliamTrueheart@users.noreply.github.com>
2022-08-22 20:23:40 -0500

    Add files via upload

--- app.ts
+++ app.ts
@@ -13,11 +13,35 @@ console.log(squareSum([1,2,3]));
 // 7 kyu
 
 export function generateShape(int: number): string { 
-    let strNum = int.toString();;
-    let line =strNum.repeat(int)
+    let line ='+'.repeat(int);
+    // i need the first iteration to be +, then \n++, etc..
+
     return line;
   }
 
-console.log(generateShape(3))
+console.log(generateShape(4))
 
-  
+// Ordered Count of Chars
+// 7 kyu
+// orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
+
+
+// Mexican Wave
+// 6 kyu
+// wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+
+export function wave(str: string): Array<string>{
+    // arr to house the iterations
+    let waveArr :string[] = [];
+    let strLen = str.length;
+    // loop through str, upperCase the index value, and push to new arr
+    for (let i=0; i<str.length; i++) {   
+      if (str[i] == ' ') {
+        continue
+      } 
+      waveArr.push(str.slice(0, i) + str[i].toUpperCase() + str.slice(i + 1, strLen))
+    }
+    return waveArr;
+  }
+ 
+  console.log(wave('two words'))
